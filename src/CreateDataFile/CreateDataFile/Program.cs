6d6adcda15d14eead62e9222012bfddcc9dd9cec
Colin Mackay <hello@xander-framework.org>
2013-01-27 21:24:48 +0000

    dictionary cleaned up. Closes #3
    Removed words from the dictionay that did not have between 4 and 8
    characters inclusive.

--- src/CreateDataFile/CreateDataFile/Program.cs
+++ src/CreateDataFile/CreateDataFile/Program.cs
@@ -22,19 +22,27 @@ namespace CreateDataFile
                 do
                 {
                     string word = new string(line.Where(char.IsLetter).ToArray());
-                    listOfWords.Add(word);
                     count++;
                     if (count%100 == 0)
                         Console.WriteLine("Read {0} words. ({1})", count, word);
+                    if ((word.Length >= 4) && (word.Length<=8))
+                        listOfWords.Add(word);
                     line = file.ReadLine();
                 } while (line != null);
 
-                Console.WriteLine("Writing the result.");
+                Console.WriteLine("Read a total of {0} words. {1} have lengths between 4 and 8.", count, listOfWords.Count);
+                Console.WriteLine("Writing the JSON result.");
                 using (TextWriter writer = new StreamWriter("words.json"))
                 {
                     JsonSerializerSettings settings = new JsonSerializerSettings();
                     var json = JsonSerializer.Create(settings);
                     json.Serialize(writer, listOfWords);
+                }
+                Console.WriteLine("Writing the TXT result.");
+                using (TextWriter writer = new StreamWriter("words.txt"))
+                {
+                    foreach(var word in listOfWords)
+                        writer.WriteLine(word);
                 }
                 Console.WriteLine("Done!");
                 Console.ReadKey();
